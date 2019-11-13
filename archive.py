b8d3d08cf2dd1c8a2d76a3b269e0950c827ce3f2
Simone Esposito <chaufnet@gmail.com>
2019-11-13 18:39:25 +0100

    Create archive.py

---
+++ archive.py
@@ -0,0 +1,26 @@
+import time
+import os
+import shutil
+
+SECONDS_IN_DAY = 24 * 60 * 60
+src = '.'
+
+
+now = time.time()
+before = now - SECONDS_IN_DAY
+
+def get_appropriate_folder(filename):
+    return time.strftime('%Y-%m', time.gmtime(os.path.getmtime(filename)))
+
+
+for fname in os.listdir(src):
+
+    if fname == 'archive.py':
+        continue
+    print('Processing ', fname)
+    src_fname = os.path.join(src, fname)
+    folder = get_appropriate_folder(src_fname)
+    os.makedirs(folder, exist_ok=True)
+
+    shutil.move(src_fname, folder)
+    print('Moved {fname} to {folder}'.format(fname=fname, folder=folder))
